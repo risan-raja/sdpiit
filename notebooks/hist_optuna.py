81f18a7f8749720bf688281fd79aa5e08837f618
Risan Raja <21f1000021@student.onlinedegree.iitm.ac.in>
2022-07-23 20:14:25 +0530

    f

--- notebooks/hist_optuna.py
+++ notebooks/hist_optuna.py
@@ -1,34 +1,9 @@
 import optuna
-import os
-
-os.environ["NEPTUNE_PROJECT"] = "mlop3n/SDP"
-os.environ[
-    "NEPTUNE_NOTEBOOK_PATH"
-] = "PycharmProjects/sdpiit/notebooks/Pipeline_components_builder.ipynb"
-import warnings
-from sklearnex import patch_sklearn
-
-patch_sklearn()
-import numpy as np
 import pandas as pd
-from category_encoders import (
-    BackwardDifferenceEncoder,
-    BaseNEncoder,
-    BinaryEncoder,
-    CatBoostEncoder,
-    CountEncoder,
-    GLMMEncoder,
-    HelmertEncoder,
-    JamesSteinEncoder,
-    LeaveOneOutEncoder,
-    MEstimateEncoder,
-    QuantileEncoder,
-    SummaryEncoder,
-    TargetEncoder,
-    WOEEncoder,
-)
-from sklearn import set_config
-from sklearn.base import clone as model_clone
+import numpy as np
+from joblib import parallel_backend
+import pickle
+import joblib
 from sklearn.cluster import *
 from sklearn.compose import *
 from sklearn.cross_decomposition import *
@@ -52,56 +27,77 @@ from sklearn.utils import *
 from sklearn.dummy import *
 from sklearn.semi_supervised import *
 from sklearn.discriminant_analysis import *
-import sklearnex, daal4py
-
-from tqdm import tqdm, trange
-from xgboost import XGBClassifier, XGBRFClassifier
-
-from sklearn.calibration import *
-
-pd.options.plotting.backend = "plotly"
-pd.options.display.max_columns = 50
-set_config(display="diagram")
-warnings.filterwarnings("ignore")
-import pickle
-from collections import defaultdict
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-from joblib import parallel_backend
-from joblib.memory import Memory
-
-sns.set()
-from pprint import pprint
-from helpers import PolynomialWrapper as PWrapper
-from helpers import NestedCVWrapper as NCVWrapper
-from helpers import ColumnSelectors
 import sklearn
-
-from helpers import DFCollection
-from helpers import plot_mean_std_max
-from helpers import CustomMetrics
-import gc
-
-CACHE_DIR = Memory(location="../data/joblib_memory/")
-# OPTUNA_DB = "postgresql+psycopg2://postgres:302492@localhost:5433/optuna"
 from REDIS_CONFIG import REDIS_URL
 
 OPTUNA_DB = REDIS_URL
 
 
-def allow_stopping(func):
-    def wrapper():
-        try:
-            value = func()
-            return value
-            # gc.collect()
-        except KeyboardInterrupt as e:
-            print("Program Stopped")
-        gc.collect()
-
-    return wrapper
 
 
-def objective(trial: optuna.trial.Trial):
-    
+
+def train_test(X, y, test_size):
+    """
+    X_train, X_test, y_train, y_test
+    """
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, random_state=10, test_size=test_size, stratify=y
+    )
+    return X_train, X_test, y_train, y_test
+
+final_data_ohe = pd.read_parquet('../data/final_data_ohe')
+
+
+target = final_data_ohe.target
+X_train, X_test, y_train, y_test = train_test(final_data_ohe.drop(['target'],axis=1), target, test_size=0.2)
+
+def objective(trial:optuna.trial.Trial):
+    categories = [True]*138
+    e_stop = {'TRUE': True, "FALSE":False}
+    hist_params = {'categorical_features': categories,
+                 'early_stopping': e_stop[trial.suggest_categorical("early_stopping",["TRUE","FALSE"])],
+                     'l2_regularization': trial.suggest_float("l2_regularization", 0.0, 0.999999),
+                     'learning_rate': trial.suggest_float("learning_rate", 0.00001, 0.999999),
+                     'loss': 'log_loss',
+                     'max_bins': 255,
+                     'max_depth': trial.suggest_int('max_depth',1,40),
+                     'max_iter': trial.suggest_int('max_iter',1,500),
+                     'max_leaf_nodes': trial.suggest_int('max_leaf_nodes',5,400),
+                     'min_samples_leaf': trial.suggest_int('min_samples_leaf',2,400),
+                     'monotonic_cst': None,
+                     'n_iter_no_change': 10,
+                     'random_state': 10,
+                     'scoring': 'f1_macro',
+                     'tol': 1e-07,
+                     'validation_fraction': trial.suggest_float("validation_fraction", 0.1, 0.3),
+                     'verbose': 0,
+                     'warm_start': False}
+    c_select = make_column_selector(pattern='one_hot_enc*|ordinal*')
+    clf = HistGradientBoostingClassifier(**hist_params)
+    ct = make_column_transformer((('passthrough',c_select)),sparse_threshold=0)
+    wf = make_pipeline(ct,clf)
+    with parallel_backend('loky'):
+        y_pred = wf.fit(X_train,y_train).predict(X_test)
+        score = sklearn.metrics.f1_score(y_test,y_pred,average='macro')
+    return score
+
+
+# objective()
+study = optuna.create_study(
+        study_name="Hist.22",
+        sampler=optuna.samplers.TPESampler(
+            warn_independent_sampling=False,
+        ),
+        storage=OPTUNA_DB,
+        direction="maximize",
+        load_if_exists=True,
+    )
+# with parallel_backend("threading"):
+study.optimize(
+    objective,
+    show_progress_bar=True,
+    gc_after_trial=True,
+    # n_jobs=2,
+    n_trials=5,
+)
+
