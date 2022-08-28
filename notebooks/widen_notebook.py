87503020f17d03b5f08a7ef0f5fa5ff7df08434e
Risan <op3ntrap@gmail.com>
2022-08-28 14:46:49 -0700

    s

---
+++ notebooks/widen_notebook.py
@@ -0,0 +1,4 @@
+from IPython.core.display import display, HTML
+display(HTML("<style>.container { width:95% !important; }</style>"))
+display(HTML("<style>.output_result { max-width:95% !important; }</style>"))
+display(HTML("<style>.prompt { display:none !important; }</style>"))
